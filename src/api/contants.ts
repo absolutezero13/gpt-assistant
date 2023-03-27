c2d923b4a4a7cc4f2e0c6e62b9de20360774cad5
ilker <ilker897@gmail.com>
2023-03-27 23:42:47 +0300

    endpoint change

--- src/api/contants.ts
+++ src/api/contants.ts
@@ -1,5 +1,5 @@
-// export const ENDPOINT =
-//   "https://us-central1-gpt-assistant-19b00.cloudfunctions.net/app/";
-
 export const ENDPOINT =
-  "http://localhost:5002/gpt-assistant-19b00/us-central1/app/";
+  "https://us-central1-gpt-assistant-19b00.cloudfunctions.net/app/";
+
+// export const ENDPOINT =
+//   "http://localhost:5002/gpt-assistant-19b00/us-central1/app/";
